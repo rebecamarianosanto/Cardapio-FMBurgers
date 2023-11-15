1ad46c6dbe2dbda19f076841bbd22b72339dda62
Rebeca Mariano <rebeca-mariano2@hotmail.com>
2023-11-15 02:03:32 -0300

    Etapa 1 do projeto aplicativo Delivery hamburgueria FMBurgers.

---
+++ app/src/main/java/com/appcatalogofm/fmburguers/MainActivity.java
@@ -0,0 +1,24 @@
+package com.appcatalogofm.fmburguers;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.SearchView;
+
+public class MainActivity extends AppCompatActivity {
+    SearchView searchView;
+
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+
+}
